9e5c8d7b8df6eb38e1e44f10846185a5b26db8cb
annikaverma <76493450+annikaverma@users.noreply.github.com>
2021-05-23 18:22:16 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -25,5 +25,24 @@ function gotPoses(results)
   if(results.length > 0)
   {
     console.log(results);
+    noseX = results[0].pose.nose.x;
+    noseY = results[0].pose.nose.y;
+    console.log("noseX = " + noseX +" noseY = " + noseY);
+
+    leftWristX = results[0].pose.leftWrist.x;
+    rightWristX = results[0].pose.rightWrist.x;
+    difference = floor(leftWristX - rightWristX);
+
+    console.log("leftWristX  = " + leftWristX  + " rightWristX = "+ rightWristX + " difference = " + difference);
   }
 }
+
+function draw() {
+background('#969A97');
+
+  document.getElementById("square_side").innerHTML = "Width And Height of a Square will be = " + difference +"px";
+  fill('#F90093');
+  stroke('#F90093');
+  square(noseX, noseY, difference);
+}
+
